d538cafd865d668b87c57ca19d0aba8d74b0dd91
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-06-01 00:34:13 +0400

    Add JS code for generating random images

---
+++ GridExamples/GridImageGallery/script.js
@@ -0,0 +1,27 @@
+const gallery = document.querySelector('.gallery');
+const overlay = document.querySelector('.overlay');
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
+
+function generateHTML([h, v]) {
+  return `
+    <div class="item h${h} v${v}">
+      <img src="images/${generateRandomNumber(12)}.jpg" />
+      <div class="item__overlay">
+        <button>View</button>
+      </div>
+    </div>
+  `;
+}
+
+function generateRandomNumber(limit) {
+  return Math.floor(Math.random() * limit) + 1;
+}
+
+const digits = Array.from({ length: 50 }, () => [
+  generateRandomNumber(4),
+  generateRandomNumber(4),
+]);
+
+const html = digits.map(generateHTML).join('');
+gallery.innerHTML = html;
